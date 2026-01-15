a623d812e2f262bd3cedfcd697282a47c8c176c2
Milton <ajaxmilton@hotmail.com>
2026-01-15 14:58:31 +0200

    Rename swagger ouptut file to swagger.json

--- swagger.js
+++ swagger.js
@@ -9,7 +9,7 @@ const doc = {
   schemes: ["https"],
 };
 
-const outputFile = "./swagger-output.json";
+const outputFile = "./swagger.json";
 const routes = ["./server.js"];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
